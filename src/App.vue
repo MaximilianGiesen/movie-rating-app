63f3ab366427cb9ee03396022e224408bea5d191
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-09-02 14:08:25 +0200

    initial commit - render out movies

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import MovieListItem from "./components/MovieListItem.vue";
+/*
+ This is an Icon that you can use to represent the stars if you like
+ otherwise you could just use a simple ⭐️ emoji, or * character.
+*/
+// import { StarIcon } from "@heroicons/vue/24/solid";
+</script>
+
+<template>
+  <!-- This is where your template goes	-->
+  <div>
+    <h1>Movie App</h1>
+    <MovieListItem />
+  </div>
+</template>
