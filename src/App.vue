87fa70b61830e31e46124451b6591311e92a9ee9
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-09-04 11:04:55 +0200

    refactoring -> rebuild project from scratch

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <script setup>
 import { items } from "./movies.json";
-/*
- This is an Icon that you can use to represent the stars if you like
- otherwise you could just use a simple ⭐️ emoji, or * character.
-*/
-// import { StarIcon } from "@heroicons/vue/24/solid";
+import { StarIcon } from "@heroicons/vue/24/solid";
+import { ref } from "vue";
+
+const movies = ref(items)
 </script>
 
 <template>
-  <!-- This is where your template goes	-->
-  <div></div>
+  <div class="app">
+    <div class="movie-list">
+      <div v-for="movie in movies" class="movie-item">
+        <div class="movie-image-wrapper">
+          <img :src="movie.image" alt="" class="movie-item-image" />
+        </div>
+        <div class="movie-item-info-wrapper">
+          <h3 class="movie-item-title">{{ movie.name }}</h3>
+          <div class="movie-genres-wrapper">
+            <span
+                v-for="genre in movie.genres"
+                :key="genre"
+                class="genre"
+            >{{ genre }}
+            </span>
+          </div>
+          <p>{{ movie.description }}</p>
+          <div class="movie-item-rating-wrapper">
+            <span
+                v-for="rating in movie.rating"
+                :key="rating"
+            >
+              <StarIcon class="w-5 h-5 text-yellow-500" />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
